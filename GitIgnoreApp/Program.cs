90507233a2952ff6a88bc7200455061843394bf8
GuardianEatos97 <5100158@mypillar7uwc.ac.za>
2023-11-30 11:12:00 +0200

    Initial Commit

---
+++ GitIgnoreApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitIgnoreApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
